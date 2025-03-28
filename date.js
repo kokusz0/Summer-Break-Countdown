c6783ff7de9dc60caea6c3bd359f112ed0891304
kokusz0 <aprogyerek3@gmail.com>
2025-03-28 14:38:40 +0100

    Update date.js

--- date.js
+++ date.js
@@ -1,13 +1,1 @@
-// date.js
 
-function getCurrentDate() {
-    const today = new Date();
-    const day = today.getDate().toString().padStart(2, '0');
-    const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Get current month (1-12)
-    const year = today.getFullYear();
-    const hours = today.getHours().toString().padStart(2, '0');
-    const minutes = today.getMinutes().toString().padStart(2, '0');
-    const seconds = today.getSeconds().toString().padStart(2, '0');
-    
-    return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`; // Format: day-month-year hour:minute:second
-}
